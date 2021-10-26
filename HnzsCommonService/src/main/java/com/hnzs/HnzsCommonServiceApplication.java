32b848745058f79724218c4f025d2152a4ee8cff
dlm5720 <571943244@qq.com>
2021-10-26 11:45:03 +0800

    Delete Jxc_exception.log

--- HnzsCommonService/src/main/java/com/hnzs/HnzsCommonServiceApplication.java
+++ HnzsCommonService/src/main/java/com/hnzs/HnzsCommonServiceApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.ComponentScan;
 
-@EnableDiscoveryClient
+//@EnableDiscoveryClient
 @SpringBootApplication
 @ComponentScan("com.hnzs.*")
 @MapperScan(basePackages= {"com.*.mapper"})
